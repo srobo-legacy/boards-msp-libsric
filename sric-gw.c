1a41b4e6535b7de14c67b3331aaf78b35ee20aba
Robert Spanton <rspanton@zepler.net>
2010-10-17 19:23:57 +0100

    Remove the enumeration code from sric-gw
    This enumeration stuff is moving to a better place: the host.  It is a
    significantly more friendly place to be debugging this stuff.

--- sric-gw.c
+++ sric-gw.c
@@ -41,20 +41,6 @@ typedef enum {
 
 /* State machine states */
 typedef enum {
-	/* Uninitialised state */
-	S_UNINIT,
-	/* Sending out reset commands to the bus */
-	S_RESET_BUS,
-	/* Pausing waiting for the token to propagate out */
-	S_TOK_PAUSE,
-	/* Sending a node its address */
-	S_SEND_ADDRESS,
-	/* Waiting for client info to be sent to host */
-	S_WAIT_INFO_TO_HOST,
-	/* Waiting for the token to be advanced */
-	S_WAIT_ADVANCE_ACK,
-	/* Waiting for the token or timeout */
-	S_WAIT_TOK,
 	/* Idle */
 	S_IDLE,
 	/* Transmitting the command the host sent us over sric */
@@ -75,160 +61,13 @@ static state_t gw_state;
 
 void sric_gw_init( void )
 {
-	if( SRIC_DIRECTOR ) {
-		gw_state = S_UNINIT;
-	} else {
-		gw_state = S_IDLE;
-	}
+	gw_state = S_IDLE;
 }
-
-#if (SRIC_DIRECTOR)
-/* Send RESET command on the SRIC bus */
-static void send_reset( void )
-{
-	sric_if.tx_lock();
-
-	sric_if.txbuf[0] = 0x7e;
-	sric_if.txbuf[SRIC_DEST] = 0;
-	sric_if.txbuf[SRIC_SRC] = sric_addr;
-	sric_if.txbuf[SRIC_LEN] = 1;
-	sric_if.txbuf[SRIC_DATA] = 0x80 | SRIC_SYSCMD_RESET;
-
-	sric_if.tx_lock();
-	sric_if.tx_cmd_start( sric_if.txbuf[SRIC_LEN] + SRIC_HEADER_SIZE, false );
-}
-
-/* Send ADDRESS_ASSIGN command on the SRIC bus */
-static void send_address( uint8_t addr )
-{
-	sric_if.tx_lock();
-
-	sric_if.txbuf[0] = 0x7e;
-	sric_if.txbuf[SRIC_DEST] = 0;
-	sric_if.txbuf[SRIC_SRC] = sric_addr;
-	sric_if.txbuf[SRIC_LEN] = 2;
-	sric_if.txbuf[SRIC_DATA] = 0x80 | SRIC_SYSCMD_ADDR_ASSIGN;
-	sric_if.txbuf[SRIC_DATA+1] = addr;
-
-	sric_if.tx_lock();
-	sric_if.tx_cmd_start( sric_if.txbuf[SRIC_LEN] + SRIC_HEADER_SIZE, true );
-}
-
-/* Send ADVANCE_TOKEN command on the SRIC bus */
-static void send_tok_advance( uint8_t addr )
-{
-	sric_if.tx_lock();
-
-	sric_if.txbuf[0] = 0x7e;
-	sric_if.txbuf[SRIC_DEST] = addr;
-	sric_if.txbuf[SRIC_SRC] = sric_addr;
-	sric_if.txbuf[SRIC_LEN] = 1;
-	sric_if.txbuf[SRIC_DATA] = 0x80 | SRIC_SYSCMD_TOK_ADVANCE;
-
-	sric_if.tx_lock();
-	sric_if.tx_cmd_start( sric_if.txbuf[SRIC_LEN] + SRIC_HEADER_SIZE, true );
-}
-
-static sched_task_t gw_task;
-static bool gw_timeout_cb( void *udata );
-
-static void gw_register_timeout( void )
-{
-	gw_task.t = 50;
-	gw_task.cb = gw_timeout_cb;
-	sched_add( &gw_task );
-}
-#endif
 
 /* State machine for this gateway */
 static void gw_fsm( gw_event_t event )
 {
 	switch( gw_state ) {
-#if (SRIC_DIRECTOR)
-		static uint8_t reset_count;
-		static uint8_t assign_addr;
-
-	case S_UNINIT:
-		/* Bus not yet initialised */
-		if( event == EV_SRIC_INIT ) {
-			sric_if.use_token( false );
-			reset_count = 0;
-			assign_addr = 5;
-
-			send_reset();
-			gw_state = S_RESET_BUS;
-		}
-		break;
-
-	case S_RESET_BUS:
-		/* Broadcast transmission's complete */
-		if( event == EV_SRIC_RX ) {
-			reset_count++;
-			if( reset_count == 10 ) {
-				token_dir_emit_first();
-				gw_register_timeout();
-
-				gw_state = S_TOK_PAUSE;
-			} else {
-				gw_register_timeout();
-			}
-		} else if( event == EV_TIMEOUT ) {
-			send_reset();
-		}
-		break;
-
-	case S_TOK_PAUSE:
-		if( event == EV_TIMEOUT ) {
-			send_address(assign_addr);
-
-			gw_state = S_SEND_ADDRESS;
-		}
-		break;
-
-	case S_SEND_ADDRESS:
-		if ( event == EV_SRIC_RX ) {
-			/* Register a timeout so that we can escape the sric state machine */
-			gw_register_timeout();
-
-		} else if( event == EV_TIMEOUT ) {
-			token_dir_drv.req();
-			send_tok_advance(assign_addr);
-
-			gw_state = S_WAIT_ADVANCE_ACK;
-		}
-		break;
-
-	case S_WAIT_INFO_TO_HOST:
-		break;
-
-	case S_WAIT_ADVANCE_ACK:
-		if( event == EV_SRIC_RX ) {
-			gw_register_timeout();
-			gw_state = S_WAIT_TOK;
-		}
-		break;
-
-	case S_WAIT_TOK:
-		if( event == EV_TIMEOUT ) {
-			if( sric_conf.token_drv->have_token() ) {
-				/* We've finished enumerating the bus */
-				sric_if.ctl( SRIC_CTL_RELEASE_TOK );
-				sric_if.use_token(true);
-				gw_state = S_IDLE;
-				break;
-			}
-
-			/* Send the next guy his address */
-			assign_addr++;
-			if( assign_addr == 127 )
-				while(1);
-			send_address(assign_addr);
-
-			gw_state = S_SEND_ADDRESS;
-		}
-		break;
-#endif
-
 	case S_IDLE:
 		/* Idling away, plotting our revenge */
 		if( event == EV_HOST_RX ) {
